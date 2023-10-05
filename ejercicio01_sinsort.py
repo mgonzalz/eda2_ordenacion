05a8f55d0833d5fa16bf958142c164a399b8788c
BobiStudio <mgonzgarc@myuax.com>
2023-10-05 19:33:12 +0200

    no vale

--- ejercicio01_sinsort.py
+++
@@ -1,8 +0,0 @@
-t = int(input()) #casos de prueba
-
-while (t != 0):
-    n = int(input()) #numero de alumnos
-    a = list(map(int, input().split())) #identificador de los alumnos
-    a = list(set(a))
-    print(len(a))
-    t= t-1
